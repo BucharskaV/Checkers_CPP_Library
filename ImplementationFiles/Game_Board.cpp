52c7b07a67c5d9df94f4896784aa1fdbafe3324d
Vladyslava <buchasrka.vladyslava@gmail.com>
2024-10-23 22:53:27 +0200

    Initial Commit

---
+++ ImplementationFiles/Game_Board.cpp
@@ -0,0 +1,59 @@
+//
+// Created by User on 10/23/2024.
+//
+#include <jni.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include "../Headers/Game_Board.h"
+
+class Board {
+private:
+    std::vector<std::vector<std::string>> board = {
+            {" ", "PBP", " ", "PBP", " ", "PBP", " ", "PBP"},
+            {"PBP", " ", "PBP", " ", "PBP", " ", "PBP", " "},
+            {" ", "PBP", " ", "PBP", " ", "PBP", " ", "PBP"},
+            {" ", " ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " ", " "},
+            {"PRP", " ", "PRP", " ", "PRP", " ", "PRP", " "},
+            {" ", "PRP", " ", "PRP", " ", "PRP", " ", "PRP"},
+            {"PRP", " ", "PRP", " ", "PRP", " ", "PRP", " "}
+    };
+    int boardWidth = 8;
+    int boardHeight = 8;
+
+public:
+    Board(int size) {}
+
+    std::string getBoardCellState(int row, int col) {
+        if (row >= 0 && row < boardHeight && col >= 0 && col < boardWidth) {
+            std::cout<<board[row][col];
+            return board[row][col];
+        }
+        return " ";
+    }
+
+    int getBoardWidth() const {
+        return boardWidth;
+    }
+
+    int getBoardHeight() const {
+        return boardHeight;
+    }
+};
+JNIEXPORT jstring JNICALL
+Java_Game_Board_getBoardCellState(JNIEnv *env, jobject obj, jint row, jint col) {
+    Board board(8);  // Create a Board object with size 8 (assuming size is passed)
+    std::string cellState = board.getBoardCellState(row, col);
+    return env->NewStringUTF(cellState.c_str());
+}
+JNIEXPORT jint JNICALL
+Java_Game_Board_getBoardWidth(JNIEnv *env, jobject obj) {
+    Board board(8);  // Create a Board object with size 8
+    return board.getBoardWidth();
+}
+JNIEXPORT jint JNICALL
+Java_Game_Board_getBoardHeight(JNIEnv *env, jobject obj) {
+    Board board(8);  // Create a Board object with size 8
+    return board.getBoardHeight();
+}
